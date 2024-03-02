a2d15ef4d0ed67c770e93e0658d5d59bad156e70
Randeep2004 <r.619@mybvc.ca>
2024-03-01 19:30:26 -0700

    finalassign

--- mark to grade/mark_to_grade_converter.js
+++ mark to grade/mark_to_grade_converter.js
@@ -38,3 +38,13 @@ backButton.addEventListener("click", function() {
 
 // Append the button to the document body or any other desired element
 document.body.appendChild(backButton);
+
+
+// Delay for the text display after 10 sec
+setTimeout(DelayText, 10000);
+
+var now = new Date();
+  var datetime = now.toLocaleString();
+
+  // Insert date and time into HTML
+  document.getElementById("datetime").innerHTML = datetime;
--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.getElementById("profile-picture").style.display = "block";
-    }, 10000);
-});
+function DelayText() {
+    const displayTextElement = document.getElementById('pictext');
+    displayTextElement.style.display = 'block';
+}
+
+// Delay for the text display after 10 sec
+setTimeout(DelayText, 10000);
+
+var now = new Date();
+  var datetime = now.toLocaleString();
+
+  // Insert date and time into HTML
+  document.getElementById("datetime").innerHTML = datetime;
